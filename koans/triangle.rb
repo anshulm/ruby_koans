583348b9f99e260c6711884280118b24645462f0
Anshul Mengi <anshulmengi@gmail.com>
2011-12-12 17:06:22 +0530

    Completed Triangle2

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,16 +14,25 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  if (a==b && a==c)
-    :equilateral
+  if (a<=0||b<=0||c<=0)
+    raise TriangleError, "Enter Valid Side Lengths"
   else
-    if (a==b || a==c || b==c)
-      :isosceles
+    if (a+b<=c||a+c<=b||b+c<=a)
+      raise TriangleError, "Enter Valid Side Lengths"
     else
-      :scalene
+      if (a==b && a==c)
+        :equilateral
+      else
+        if (a==b || a==c || b==c)
+          :isosceles
+        else
+          :scalene
+        end
+      end
     end
   end
 end
+
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
